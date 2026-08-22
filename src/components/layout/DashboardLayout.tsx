1e2384951cce93ce07d60af4a530b05b0e4d7365
Piyush Ramteke <piyu.143247@gmail.com>
2026-08-23 00:38:18 +0530

    Use Supabase navbar in dashboard layout

--- src/components/layout/DashboardLayout.tsx
+++ src/components/layout/DashboardLayout.tsx
@@ -2,25 +2,18 @@
 
 import { useState } from "react";
 import { Sidebar } from "./Sidebar";
-import { Navbar } from "./Navbar";
+import { SupabaseNavbar } from "./SupabaseNavbar";
 
 export function DashboardLayout({ children }: { children: React.ReactNode }) {
   const [sidebarOpen, setSidebarOpen] = useState(false);
 
   return (
     <div className="flex h-screen bg-vault-bg overflow-hidden">
-      {/* Sidebar - Controlled by state for mobile */}
       <Sidebar isOpen={sidebarOpen} setIsOpen={setSidebarOpen} />
-
-      {/* Main Content Area */}
       <div className="flex-1 flex flex-col min-w-0 overflow-hidden">
-        <Navbar setSidebarOpen={setSidebarOpen} />
-        
+        <SupabaseNavbar setSidebarOpen={setSidebarOpen} />
         <main className="flex-1 overflow-y-auto p-4 lg:p-8 scroll-smooth">
-          {/* Max width container for readable content */}
-          <div className="mx-auto max-w-7xl">
-            {children}
-          </div>
+          <div className="mx-auto max-w-7xl">{children}</div>
         </main>
       </div>
     </div>
